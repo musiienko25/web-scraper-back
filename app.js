763c37564feada87331cd35e23780f775e0b894b
musiienko25 <devmusiienko@gmail.com>
2024-07-15 19:24:45 +0300

    minor changes

--- app.js
+++ app.js
@@ -53,7 +53,9 @@ app.get("/api/scrape", async (req, res) => {
     res.json(bidDetails);
   } catch (error) {
     console.error("Error scraping data:", error);
-    res.status(500).json({ error: "Error fetching data from external source" });
+    res
+      .status(500)
+      .json({ error: "Error fetching data from external source." });
   }
 });
 
